004f4dde8ebafc9c239963f3cff2fbd730daf3cd
Sindre Wetjen <sindre@wetjen.no>
2015-01-27 22:46:16 +0100

    Underlying bot changes, including a fix of the update_map logic
    The update function wasn't an update, as rather the whole map.
    This means you need to take the relative complement of your
    existing regions from the update after the update is done.
    
    Include some more ignores for debugging.
    
    Conflicts:
    	world-map.rb

--- world-map.rb
+++ world-map.rb
@@ -11,6 +11,7 @@ class WorldMap
     @regions = []
     @my_regions = []
     @my_super_regions = []
+    @visible_regions = []
     @settings = settings
     @round = round
   end
@@ -22,6 +23,47 @@ class WorldMap
     when 'neutral' then :neutral
     else raise(format('unsupported players: %s', player_name))
     end
+  end
+
+  def update_map(raw_line)
+    @round.update_map()
+    @visible_regions = []
+    while raw_line.length >= 3
+      action = raw_line.shift(3)
+      region_id, player_name, forces = action[0].to_i, action[1], action[2].to_i
+      region = @regions[region_id - 1]
+      now_owned_by = owned_by_symbol(player_name)
+
+      region.forces = forces
+      if region.owned_by == :me
+        lost_region(region)  if now_owned_by != :me
+      else
+        claim_region(region) if now_owned_by == :me
+      end
+
+      @visible_regions << region
+    end
+
+    (@my_regions - @visible_regions).each{|region| lost_region(region)}
+  end
+
+  def path_between_regions(start_region_id, end_region_id)
+    @regions[start_region_id - 1].path_to{|region| end_region_id == region.id}
+  end
+
+  def map_to_string()
+    " == Regions == \n" +
+    @regions.map do |region|
+      region.id.to_s + (region.owned_by == :me ? "y" : "o") +
+      region.forces.to_s +
+      " -> (" + region.neighbours.map{|neighbour| neighbour.id}.join(", ") + ")"
+    end.join("\n") + "\n" +
+    " == My Regions == \n" +
+    @my_regions.map do |region|
+      region.id.to_s + (region.owned_by == :me ? "y" : "o") +
+      region.forces.to_s +
+      " -> (" + region.neighbours.map{|neighbour| neighbour.id}.join(", ") + ")"
+    end.join("\n")
   end
 
   def setup_line(raw_line)
@@ -47,6 +89,7 @@ class WorldMap
         end
       end
 
+      @regions.each{|r| r.owned_by = :neutral}
     when 'neighbors' then
       raw_line.each_index do |index|
         if index.even?
@@ -76,57 +119,29 @@ class WorldMap
     end
   end
 
-  def claim_region(region_id)
-    @regions[region_id - 1].owned_by = :me
-    @my_regions << @regions[region_id - 1]
-  end
+  private
 
-  def update_map(raw_line)
-    @round.update_map()
-    while raw_line.length >= 3
-      action = raw_line.shift(3)
-      region_id, player_name, forces = action[0].to_i, action[1], action[2].to_i
-      region = @regions[region_id - 1]
-      now_owned_by = owned_by_symbol(player_name)
+  def claim_region(region)
+    region = @regions[region - 1] unless region.kind_of?(Region)
+    region.owned_by = :me
+    @my_regions << region
 
-      if region.owned_by == :me
-        if now_owned_by != :me
-          @my_regions.delete(region)
-          if @my_super_regions.include?(region.super_region)
-            @my_super_regions.delete(region.super_region)
-            @round.super_region_changes[:lost].push(region.super_region)
-          end
-        end
-      else
-        if now_owned_by == :me
-          @my_regions.push(region)
-          if region.super_region.regions.index do |r|
-               r != region and r.owned_by != :me
-             end.nil?
-          then
-            @my_super_regions.push(region.super_region)
-            @round.super_region_changes[:gained].push(region.super_region)
-          end
-        end
-      end
-
-      region.owned_by = now_owned_by
-      region.forces = forces
+    # Check if we also claimed the super_region
+    unless region.super_region.regions.any?{|r| r.owned_by != :me}
+      @my_super_regions.push(region.super_region)
+      @round.super_region_changes[:gained].push(region.super_region)
     end
   end
 
-  def map_to_string()
-    " == Regions == \n" +
-    @regions.map do |region|
-      region.id.to_s + (region.owned_by == :me ? "y" : "o") +
-      region.forces.to_s +
-      " -> (" + region.neighbours.map{|neighbour| neighbour.id}.join(", ") + ")"
-    end.join("\n") + "\n" +
-    " == My Regions == \n" +
-    @my_regions.map do |region|
-      region.id.to_s + (region.owned_by == :me ? "y" : "o") +
-      region.forces.to_s +
-      " -> (" + region.neighbours.map{|neighbour| neighbour.id}.join(", ") + ")"
-    end.join("\n")
+  def lost_region(region)
+    region = @regions[region_id - 1] unless region.kind_of?(Region)
+    region.owned_by = :enemy
+    @my_regions.delete(region)
+
+    # Check if we also lost a super-region
+    if @my_super_regions.include?(region.super_region)
+      @my_super_regions.delete(region.super_region)
+      @round.super_region_changes[:lost].push(region.super_region)
+    end
   end
 end
